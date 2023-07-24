ef0f2947fc5281a3a061cb95f2b20836c9c43f3f
Komalgulati05 <79501119+Komalgulati05@users.noreply.github.com>
2023-07-24 13:56:38 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (66.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -4,8 +4,7 @@ class Solution {
         int k=0,temp=0;
         for(int i=0;i<=j;){
             if(nums[i]==val){               
-                 nums[i]=nums[j];
-               
+                 nums[i]=nums[j];               
                  j--;
                 k++;
             }
