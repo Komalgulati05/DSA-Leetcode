17a666b5a616c1205ed9ab54ec79364b0362b952
Komalgulati05 <79501119+Komalgulati05@users.noreply.github.com>
2023-07-27 14:04:17 +0530

    Time: 10 ms (53.34%), Space: 43.3 MB (16.44%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        if(x==0){
+            return true;
+        }
+
+        String str=Integer.toString(x);
+        int n=str.length();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+           if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+  }
+}
